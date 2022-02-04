d35c94e301b1855ee899b7b8608dfee241592a82
Sung hyuk Oh <dhquffl@gmail.com>
2022-02-04 16:03:56 -0500

    login function added

--- client/src/components/Home/Login.tsx
+++ client/src/components/Home/Login.tsx
@@ -1,18 +1,41 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
-  const [userInfo, setUserInfo] = useState<{}>({});
+  const [userInfo, setUserInfo] = useState<{
+    username: string;
+    password: string;
+  }>({
+    username: '',
+    password: '',
+  });
+
+  const usernameHandler = (e) => {
+    setUserInfo({
+      ...userInfo,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const passwordHandler = () => {};
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    setUserInfo({
+      ...userInfo,
+    });
+  };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <h1>Login</h1>
       <div>
         <label htmlFor="usename">Username</label>
-        <input type="text" id="username" />
+        <input type="text" id="username" onChange={usernameHandler} />
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input type="password" id="password" onChange={passwordHandler} />
       </div>
       <div>
         <button type="submit">Login</button>
