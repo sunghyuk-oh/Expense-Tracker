dcef502270b801dd3bed8e8286270db64cd3e011
Sung hyuk Oh <dhquffl@gmail.com>
2022-02-07 14:43:16 -0500

    login functionality edited

--- server/app.js
+++ server/app.js
@@ -10,4 +10,28 @@ app.use(cors());
 app.use(express.json());
 app.use(urlencoded());
 
+const users = [
+  {
+    username: 'johndoe',
+    password: 'password',
+  },
+];
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(
+    (user) => user.username == username && user.password == password
+  );
+
+  if (user) {
+    // generate the json web token
+    const token = jwt.sign({ username: user.username }, 'SECRETKEY');
+
+    res.json({ success: true, token: token });
+  } else {
+    // response with not authenticated
+    res.json({ success: false, message: 'Not authenticated' });
+  }
+});
 app.listen(3000, () => console.log('Server is running...'));
