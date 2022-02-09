4443bf014947d02ed3affee1aeea4676836b3d45
Sung hyuk Oh <dhquffl@gmail.com>
2022-02-09 16:46:17 -0500

    .

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -11,6 +11,11 @@ import rootReducer from '../src/store/reducers';
 
 const store = createStore(rootReducer, composeWithDevTools());
 
+const token = localStorage.getItem('jsonwebtoken');
+if (token) {
+  store.dispatch({ type: 'ON_LOGIN' });
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -22,7 +27,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
