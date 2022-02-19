0a9269262d7e2f9cc812733cbbc96cb5f0ea28d9
Sung hyuk Oh <dhquffl@gmail.com>
2022-02-18 19:40:45 -0500

    edited

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -42,11 +42,6 @@ const Home = () => {
             <input type="text" id="" />
           </div>
 
-          <div>
-            <label htmlFor=""></label>
-            <input type="text" id="" />
-          </div>
-
           <button>Add Expense</button>
         </form>
       </section>
